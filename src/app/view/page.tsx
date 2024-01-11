8a6b0834b3c1e0e3374a65a4d0cde4413eb5b7b7
jashwanth0712 <jashwanth0712@gmail.com>
2024-01-11 17:47:15 +0530

    removed react-json-view

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -2,33 +2,39 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'; // Assuming you are using react-router-dom for navigation
 import React, { useEffect, useState } from 'react';
-import ReactJson from 'react-json-view'; // Import react-json-view
 
 export const revalidate = 0;
-const ViewSpecies: React.FC = ({ }) => {
-    const [species, setSpecies] = useState<any | null>(null);
+const ViewSpecies: React.FC= ({ }) => {
+    const [species, setSpecies] = useState< any | null>(null);
     const router = useRouter();
 
     const searchParams = useSearchParams()
-    const id = searchParams?.get('id') ?? ''
+    const id  = searchParams?.get('id') ?? ''
+
+
+
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(`/api/species/${id}`, {
+                const response = await fetch(`/api/species/${id}`,{
                     next: { revalidate: 0 },
-                });
+                  });
                 console.log(response)
                 const data = await response.json();
                 if (response.status === 200) {
                     setSpecies(data.speciesData);
                 } else {
                     console.error('Error fetching species data:', data.error);
+                    // Redirect to an error page or handle as needed
+                    // router.push('/error');
                 }
             } catch (error) {
                 console.error('Error fetching species data:', error);
+                // Redirect to an error page or handle as needed
+                // router.push('/error');
             }
         };
 
@@ -36,21 +42,13 @@ const ViewSpecies: React.FC = ({ }) => {
     }, [id, router]);
 
     if (!species) {
-        return <div>Loading...</div>;
+        return <div>Loading...</div>; // You might want to show a loading state
     }
 
     return (
         <div className=" pt-10 px-5">
-            <h1 className="text-4xl font-semibold mb-7">Details of  {species.body.title}</h1>
-            {/* Use ReactJson to display JSON data */}
-            <ReactJson
-                src={species}
-                name={false} // Hide the "root" name
-                displayDataTypes={false} // Hide the data types at the end of the keys
-                theme="monokai" // You can choose a different theme if you prefer
-                indentWidth={4} // Number of spaces to use for indentation
-                enableClipboard={false} // Disable clipboard copy
-            />
+            <h1 className="text-4xl font-semibold mb-7">View Species</h1>
+            <pre>{JSON.stringify(species, null, 4)}</pre>
         </div>
     );
 };
